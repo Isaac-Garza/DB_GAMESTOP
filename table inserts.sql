3010e0572fa9743a5b6ed139d475c3648447a919
ICHeadache <55867190+ICHeadache@users.noreply.github.com>
2020-12-08 21:58:14 -0800

    Added Game titles into db and console

--- table inserts.sql
+++ table inserts.sql
@@ -29,14 +29,25 @@
 -- (16,"2K Games",2),
 -- (17,"Warner Brothers Interactive Entertainment",2);
 
-insert into `game_titles` (game_id,game_name,game_rating,game_genre,game_release_date,games_esrb_rating,game_price,game_condition,game_publisher) values
-(1,"Grand Theft Auto V",5,"Shooting","9/17/13","Mature",19.99,2,15),
-(2,"Legend of Zelda",4,"RPG","3/3/2017","Everyone",44.99,1,2),
-(3,"Skyrim",4,"RPG","11/11/11","Mature",19.99,3,14),
-(4,"Call of Duty Modern Warfare",4,"Shooter","10/04/2019","Mature",60.00,1,1),
-(5,"NBA 2K20",3,"Sports","11/06/2019","Everyone",25.00,3,9),
-(6,"Madden NFL 20",4,"Sports","08/04/2019","Everyone",60.00,2,8),
-(7,"Borderlands 3",3,"Shooter","9/13/2019","Mature",15.97,3,16),
-(8,"Mortal Kombat 11",3,"Fighting","4/21/2019","Mature",20.49,3,17),
-(9,"Super Smash Bros",5,"Fighting","12/7/2018","Everyone",59.95,1,2),
-(10,"Animal Crossing",5,"RPG","3/20/2020","Everyone",56.99,1,2);
+-- insert into `game_titles` (game_id, game_name, game_rating, game_genre, game_release_date, games_esrb_rating, game_price, game_condition, game_publisher) values
+-- (1,"Grand Theft Auto V",5,"Shooting","2013-09-17","Mature",19.99,2,15),
+-- (2,"Legend of Zelda",4,"RPG","2017-03-03","Everyone",44.99,1,2),
+-- (3,"Skyrim",4,"RPG","2011-11-11","Mature",19.99,3,14),
+-- (4,"Call of Duty Modern Warfare",4,"Shooter","2019-10-04","Mature",60.00,1,1),
+-- (5,"NBA 2K20",3,"Sports","2019-11-06","Everyone",25.00,3,9),
+-- (6,"Madden NFL 20",4,"Sports","2019-08-04","Everyone",60.00,2,8),
+-- (7,"Borderlands 3",3,"Shooter","2019-09-13","Mature",15.97,3,16),
+-- (8,"Mortal Kombat 11",3,"Fighting","2019-04-21","Mature",20.49,3,17),
+-- (9,"Super Smash Bros",5,"Fighting","2018-12-07","Everyone",59.95,1,2),
+-- (10,"Animal Crossing",5,"RPG","2020-03-20","Everyone",56.99,1,2);
+
+console_id,console_name,console_price,platform_id,console_condition
+(1,"Xbox One X",499,1,4),
+(2,"Playstation 4",299,2,2),
+(3,"Playstation 5",499,2,1),
+(4,"Nintendo Switch",299,3,1),
+(5,"Playstation 4 Pro",399,2,3),
+(6,"Xbox Series X",499,1,1),
+(7,"Xbox Series S",299,1,2),
+(8,"Xbox One S",359,1,1),
+(9,"Nintendo Switch Lite",199,3,3);
