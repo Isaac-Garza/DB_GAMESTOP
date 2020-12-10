4aa77c08f72cbd094e06fe73c928f7dbf01c31df
lmanuelfigueroa <lmanuelfigueroa@gmail.com>
2020-12-09 23:10:27 -0800

    Update sp_games_price_range.sql

--- sp_games_price_range.sql
+++ sp_games_price_range.sql
@@ -1,13 +1,9 @@
 use gamestop;
 
-select game_name as "Name", game_price as "Price", game_rating as "Stars", game_genre as "Genre",
-games_esrb_rating "Rated", condition_type as "Condition", platform_name as "Platform", console_name as "Console"
-from game_titles join publisher on game_titles.game_publisher=publisher.game_publisher_id 
-join platforms on publisher.platform_id=platforms.platform_id
-join consoles on consoles.platform_id=platforms.platform_id
-join conditions on game_titles.game_condition=conditions.condition_id where game_price > 50 and game_price <80;
+DROP PROCEDURE IF EXISTS `sp_games_price_range`;
+DELIMITER //
 
-/*CREATE PROCEDURE `sp_games_price_range` (IN min float, max float)
+CREATE PROCEDURE `sp_games_price_range` (IN min float, max float)
 BEGIN
 select game_name as "Name", game_price as "Price", game_rating as "Stars", game_genre as "Genre",
 games_esrb_rating "Rated", condition_type as "Condition", platform_name as "Platform", console_name as "Console"
@@ -15,4 +11,6 @@ from game_titles join publisher on game_titles.game_publisher=publisher.game_pub
 join platforms on publisher.platform_id=platforms.platform_id
 join consoles on consoles.platform_id=platforms.platform_id
 join conditions on game_titles.game_condition=conditions.condition_id where game_price > min and game_price <max;
-END*/
+END//
+
+call sp_games_price_range(50,80);
