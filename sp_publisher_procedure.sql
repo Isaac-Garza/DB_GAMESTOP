f1911206dc56f32b01087e630bac5a5c583737f1
lmanuelfigueroa <lmanuelfigueroa@gmail.com>
2020-12-09 23:12:09 -0800

    Update sp_publisher_procedure.sql

--- sp_publisher_procedure.sql
+++ sp_publisher_procedure.sql
@@ -1,13 +1,14 @@
 use gamestop;
 
-select game_name as "Game", publisher.publisher_name as "Publisher" from game_titles
-join publisher on game_titles.game_publisher= publisher.game_publisher_id where
-publisher.publisher_name="EA";
+DROP PROCEDURE IF EXISTS `sp_find_games_by_publisher`;
+DELIMITER //
 
-/*CREATE PROCEDURE `sp_find_games_by_publisher` (IN publisher varchar(64))
+CREATE PROCEDURE `sp_find_games_by_publisher` (IN publisher varchar(64))
 BEGIN
 select game_name as "Game", publisher.publisher_name as "Publisher" from game_titles
 join publisher on game_titles.game_publisher= publisher.game_publisher_id where 
 publisher.publisher_name=publisher;
-END*/
+END//
+
+call sp_find_games_by_publisher("EA");
 
