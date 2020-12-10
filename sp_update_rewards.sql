f405350f69bffac3ab7c1155b96c55816de54268
ICHeadache <55867190+ICHeadache@users.noreply.github.com>
2020-12-09 19:42:37 -0800

    Update sp_update_rewards.sql

--- sp_update_rewards.sql
+++ sp_update_rewards.sql
@@ -1,10 +1,11 @@
 DROP PROCEDURE IF EXISTS `sp_update_rewards`;
+use gamestop;
 DELIMITER //
 
 CREATE PROCEDURE `sp_update_rewards` (IN member_ident int, IN new_tier int)
 	BEGIN 
-		UPDATE rewards_member SET reward_member.member_tier = newPrice 
-        WHERE rewards_member.member_id = member_ident; 
+		UPDATE reward_member SET reward_member.member_tier = new_tier 
+        WHERE reward_member.member_id = member_ident; 
 END//
 
-CALL sp_update_rewards(1, 1);
+CALL sp_update_rewards(1, 3);
