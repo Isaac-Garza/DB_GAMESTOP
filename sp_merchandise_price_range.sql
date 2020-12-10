8affa653346c6c55d2ede01fdf125164ea59963f
lmanuelfigueroa <lmanuelfigueroa@gmail.com>
2020-12-09 23:11:34 -0800

    Update sp_merchandise_price_range.sql

--- sp_merchandise_price_range.sql
+++ sp_merchandise_price_range.sql
@@ -1,10 +1,13 @@
 use gamestop;
+DROP PROCEDURE IF EXISTS `sp_merchandise_price_range`;
+DELIMITER //
 
-select merchandise_name as "Name", price as "Price"
-from merchandise where price > 50 and price <80;
+CREATE PROCEDURE `sp_merchandise_price_range` (IN min float, max float)
+	BEGIN
+		select
+			merchandise_name as "Name", price as "Price"
+		from merchandise 
+        where price > min and price <max;
+END//
 
-/*CREATE PROCEDURE `sp_merchandise_price_range` (IN min float, max float)
-BEGIN
-select merchandise_name as "Name", price as "Price"
-from merchandise where price > min and price <max;
-END*/
+call sp_merchandise_price_range(50,80);
