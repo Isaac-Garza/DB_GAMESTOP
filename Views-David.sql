70c2409e2db9a1633f695eb2f372fd3bfbf82ccc
David <davidlam021@gmail.com>
2020-12-09 20:12:03 -0800

    Finished my views and stored procedures

---
+++ Views-David.sql
@@ -0,0 +1,23 @@
+-- DROP VIEW IF EXISTS vw_game_title;
+-- DELIMITER //
+
+-- CREATE VIEW vw_game_title AS
+-- 	SELECT game_name
+--     FROM game_titles;
+-- SELECT * FROM vw_game_title;
+
+-- DROP VIEW IF EXISTS vw_accessories;
+-- DELIMITER //
+
+-- CREATE VIEW vw_accessories AS
+-- 	SELECT access_name
+--     FROM accessories;
+-- SELECT * FROM vw_accessories;
+
+-- DROP VIEW IF EXISTS vw_merchandise;
+-- DELIMITER //
+
+-- CREATE VIEW vw_merchandise AS
+-- 	SELECT merchandise_name
+--     FROM merchandise;
+-- SELECT * FROM vw_merchandise;
