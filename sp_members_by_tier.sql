9a0171be50a298fb212af382c925fe5943f34b17
lmanuelfigueroa <lmanuelfigueroa@gmail.com>
2020-12-09 23:11:02 -0800

    Update sp_members_by_tier.sql

--- sp_members_by_tier.sql
+++ sp_members_by_tier.sql
@@ -1,13 +1,13 @@
 use gamestop;
 
-select concat(first_name, " ",middle_name ," ",last_name) as "Name", telephone as "Phone", 
-email as "Email", member_tier as "Tier", reward_points as "Points" 
-from reward_member join rewards_type on reward_member.member_tier=rewards_type.reward_id;
+DROP PROCEDURE IF EXISTS `sp_members_by_tier`;
+DELIMITER //
 
-
-/*CREATE PROCEDURE `sp_members_by_tier` (IN tier int)
+CREATE PROCEDURE `sp_members_by_tier` (IN tier int)
 BEGIN
 select concat(first_name, " ",middle_name ," ",last_name) as "Name", telephone as "Phone", 
 email as "Email", member_tier as "Tier", reward_points as "Points" 
-from reward_member join rewards_type on reward_member.member_tier=rewards_type.reward_id;
-END*/
+from reward_member join rewards_type on reward_member.member_tier=rewards_type.reward_id where member_tier = tier;
+END//
+
+call sp_members_by_tier(2);
