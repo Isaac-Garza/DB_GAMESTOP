d348c3b0c730b52e528483db4c3ba3816842dc9e
lmanuelfigueroa <lmanuelfigueroa@gmail.com>
2020-12-09 23:09:52 -0800

    Update sp_games_by_platform.sql
    updates

--- sp_games_by_platform.sql
+++ sp_games_by_platform.sql
@@ -1,12 +1,13 @@
 use gamestop;
 
-select game_name as "Game", platforms.platform_name as "Platform" from game_titles
-join publisher on game_titles.game_publisher= publisher.game_publisher_id 
-join platforms on publisher.platform_id=platforms.platform_id where platform_name="Sony";
+DROP PROCEDURE IF EXISTS `sp_find_games_by_platform`;
+DELIMITER //
 
-/*CREATE PROCEDURE `sp_find_games_by_platform` (IN platform varchar(64))
+CREATE PROCEDURE `sp_find_games_by_platform` (IN platform varchar(64))
 BEGIN
 select game_name as "Game", platforms.platform_name as "Platform" from game_titles
 join publisher on game_titles.game_publisher= publisher.game_publisher_id 
-join platforms on publisher.platform_id=platforms.platform_id where platform_name=platform
-END*/
+join platforms on publisher.platform_id=platforms.platform_id where platform_name=platform;
+END//
+
+call sp_find_games_by_platform("Sony");
